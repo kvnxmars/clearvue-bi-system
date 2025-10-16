c3f2ae4d68c3b27e403b5915e667006251b6ab16
Luyanandi <nandimhlng@gmail.com>
2025-10-16 12:08:42 +0200

    Consumer

--- etl_scripts/streaming_etl/kafka_consumer.py
+++ etl_scripts/streaming_etl/kafka_consumer.py
@@ -11,3 +11,28 @@ print("Consumer started... listening for payment transactions.")
 
 for message in consumer:
     print("Received:", message.value)
+
+"""import json
+from kafka import KafkaConsumer
+from pymongo import MongoClient
+
+mongo_client = MongoClient("") #mongo uri required
+db = mongo_client[""] #required
+bi_collection = db[""] #required 
+
+consumer = KafkaConsumer(
+    "clearvue_stream",
+    bootstrap_servers="localhost:9092",
+    auto_offset_reset="earliest",
+    enable_auto_commit=True,
+    group_id="clearvue-bi-group",
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+)
+
+print("Listening for messages...")
+
+for message in consumer:
+    data = message.value
+    print(f"Received: {data}")
+    bi_collection.insert_one(data) """
+
